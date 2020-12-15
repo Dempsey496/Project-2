ab7ef583b4311457af5db99a9df3d60ab86648a9
Austin Ellis <Austin@Janets-iMac.local>
2020-12-14 21:23:01 -0500

    Validate error and allowNull error. Changed to notNull and commented out validate

--- models/gift.js
+++ models/gift.js
@@ -1,16 +1,39 @@
-module.exports = function (sequelize, DataTypes) {
-  const Gift = sequelize.define("Gift", {
-    giftName: DataTypes.STRING,
-    giftFor: DataTypes.STRING,
-    giftMaxPrice: DataTypes.STRING
-  });
-
-  Gift.associate = function (models) {
-    Gift.belongsTo(models.List, {
-      // through: "Lists",
-      foreignKey: "GiftId",
+module.exports = function(sequelize, DataTypes) {
+    const Gift = sequelize.define("Gift", {
+        giftName: {
+            type: DataTypes.STRING,
+            notNull: true,
+            // validate: {
+            //     notNull: {
+            //         msg: 'Please enter the name of the gift'
+            //     }
+            // }
+        },
+        giftFor: {
+            type: DataTypes.String,
+            notNull: true,
+            // validate: {
+            //     notNull: {
+            //         msg: 'Please enter the name of the receiver'
+            //     }
+            // }
+        },
+        giftMaxPrice: {
+            type: DataTypes.INTEGER,
+            notNull: true,
+            // validate: {
+            //     notNull: {
+            //         msg: 'Please enter the max price of the gift'
+            //     }
+            // }
+        }
     });
-  };
 
-  return Gift;
+    Gift.associate = function(models) {
+        Gift.belongsTo(models.Gift, {
+            foreignKey: "GiftId",
+        });
+    };
+
+    return Gift;
 };
