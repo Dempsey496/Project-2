02b603a400aac60d8b33e9fe9402c786af8fed83
Dempsey FP <dempseyfinley@gmail.com>
2020-12-16 19:43:58 -0500

    links between handlebars

--- public/assets/js/lists.js
+++ public/assets/js/lists.js
@@ -1,22 +1,24 @@
 $(document).ready(function() {
-    $("#view-lists-button").on("click", function(event) {
-        var id = $(this).data("id");
-        console.log("you viewed the lists!");
-        
-      });
-
-    $(".create-list-submit").on("click", function(event) {
-      var listCreator = $("#list-creator").val();
-      var listName = $("#list-name").val();
-      $.ajax({
-        url:"/api/lists",
-        method: "POST",
-        data: {name:listName, creator:listCreator}
-      }).then(function(response) {
-        console.log(response)
-        window.location.href="/lists";
-      })
-    });
-
-
+  $(".create-list-submit").on("click", function(event) {
+    var listCreator = $("#list-creator").val();
+    var listName = $("#list-name").val();
+    $.ajax({
+      url:"/api/lists",
+      method: "POST",
+      data: {name:listName, creator:listCreator}
+    }).then(function(response) {
+      console.log(response)
+    })
+  });
+  $("#delete-button").on("click", function(event) {
+    $.ajax({
+      url:"/api/lists/:id",
+      method: "DELETE",
+      where: {
+        id: req.params.id,
+      },
+    }).then(function(response) {
+      console.log(response)
+    })
+  });
 });
